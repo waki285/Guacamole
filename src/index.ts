845cafae37f8bde4035017a197bd006dc1ea5490
waki285 <suzuneu@suzuneu.com>
2021-12-27 04:32:00 +0000

    ✨ feat: add axios and audio request

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import { Readable } from "stream";
 import disbut from "discord-buttons";
 import { okRecord, cancelRecord } from "./static";
 import { VoiceConnection } from "discord.js";
-import { createReadStream, createWriteStream } from "fs"
+import { createWriteStream } from "fs"
 import { config } from "dotenv";
 config();
+import axios, { AxiosResponse } from "axios";
+
 
 const client:Client = new Client();
 const prefix:"r!" = "r!";
@@ -36,7 +38,13 @@ client.on("message", async (message:Message) => {
     const vc:VoiceChannel | null = message.member.voice.channel;
     if (!vc) return message.reply("先にボイスチャンネルに参加してください！");
     const connection: VoiceConnection = await vc.join();
-    connection.play(createReadStream("../assets/recording.mp3"));
+    const voice: AxiosResponse<any, Readable> = await axios({
+      url: "https://www.google.com/speech-api/v1/synthesize?text=recording%20in%20progress&nc=mpeg&lang=en&speed=0.5&client=lr-language-tts",
+      responseType: "stream",
+      method: "get"
+    });
+    //const dispatcher = connection.play("../assets/recording.mp3", { volume: 1 });
+    connection.play(voice.data)
     const confirmMsg: Message = await message.channel.send({ content: `**警告:** 録音を開始すると、このVCにいる全ての人の音声が録音されます。\n本当に録音しますか?`, buttons: [okRecord, cancelRecord]});
     confirmMsg.awaitButtons((c) => c.clicker.user.id === message.author.id, { max: 1 })
   }
