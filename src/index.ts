405a3dfd4cf384bb0f1f5ce4ecf6c9b5a41bea78
waki285 <suzuneu@suzuneu.com>
2021-12-27 01:03:24 +0000

    ✨ feat: add discord-buttons

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Client, Message, VoiceChannel } from "discord.js";
 import { Readable } from "stream";
 import disbut from "discord-buttons";
 import { okRecord, cancelRecord } from "./static";
+import { VoiceConnection } from "discord.js";
+import { createReadStream, createWriteStream } from "fs"
 
 const client:Client = new Client();
 const prefix:"r!" = "r!";
@@ -31,6 +33,11 @@ client.on("message", async (message:Message) => {
   if (command === "start") {
     const vc:VoiceChannel | null = message.member.voice.channel;
     if (!vc) return message.reply("先にボイスチャンネルに参加してください！");
+    const connection: VoiceConnection = await vc.join();
+    connection.play(createReadStream("../assets/recording.mp3"));
     const confirmMsg: Message = await message.channel.send({ content: `**警告:** 録音を開始すると、このVCにいる全ての人の音声が録音されます。\n本当に録音しますか?`, buttons: [okRecord, cancelRecord]});
+    confirmMsg.awaitButtons((c) => c.clicker.user.id === message.author.id, { max: 1 })
   }
-})
+});
+
+client.login(process.env.BOT_TOKEN);
